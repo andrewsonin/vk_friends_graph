019d736ccdbd4ed2c9649862684f347633b0cf0c
Andrew Sonin <sonin.cel@gmail.com>
2017-03-11 03:17:59 +0300

    Add files via upload

--- vk_friends_graph.py
+++ vk_friends_graph.py
@@ -2,9 +2,7 @@ import requests
 import matplotlib.pyplot as plt
 import networkx as nx
 from time import sleep
-print('This program is designed to build a graph of the friendship relations between users of social network VK.\n'
-      'ATTENTION!!! If you choose a depth of search greater than 1, be ready to face a lack of RAM.\n'
-      'If you have a very fast computer, please, input nonzero delay. Otherwise antiDoS system may reject you.\n')
+print('This program is designed to build a graph of the friendship relations between users of social network VK.\n')
 
 
 def get_friends_dict(user_id=1):
@@ -22,12 +20,12 @@ def add_account_to_graph(graph, id):
 def graph_builder(graph, id, depth=1, delay=float(0), inscription='', enrich_inscription=True):
     add_account_to_graph(graph, id)
     sleep(delay)
-    if enrich_inscription:
-        inscription = str(len(graph.nodes()))
     if depth == 1:
         return
+    if enrich_inscription:
+        inscription = str(len(graph.nodes()))
     iterator = 1
-    for friend_id in graph.nodes():
+    for friend_id in graph.nodes()[1:]:
         iteration = str(iterator) + '/' + inscription
         print('Iteration:', iteration)
         graph_builder(graph, friend_id, depth - 1, delay, iteration, False)
@@ -39,21 +37,24 @@ VERSION = '5.62'
 
 print('Please, input ID of some VK user:')
 my_id = int(input())
-print('Please, input a depth of search:')
+print('Please, input a depth of search.\nATTENTION!!! If you choose a depth of search greater than 1, be ready to face '
+      'a lack of RAM.')
 my_depth = int(input())
 if my_depth < 1:
     my_depth = 1
     print('Invalid value. The depth was changed to 1.')
-print('Please, input a time delay in seconds:')
+print('Please, input a time delay in seconds.\nIf you have a very fast computer, please, input nonzero delay. Otherwise'
+      ' antiDoS system may reject you.')
 my_delay = float(input())
 
 my_graph = nx.Graph()
 graph_builder(my_graph, my_id, my_depth, my_delay)
 
 print('Calculating space configuration of vertexes…')
-positions = nx.spring_layout(my_graph)
+positions = nx.circular_layout(my_graph)
 edges = [element for element in my_graph.edges(data=True)]
 
+print('Full number of accounts involved:', len(my_graph.nodes()))
 print('Drawing a graph…')
 nx.draw_networkx_nodes(my_graph, positions, node_shape='s', node_size=500, node_color='y')
 nx.draw_networkx_labels(my_graph, positions, font_size=4, font_family='sans-serif', font_color='r')
