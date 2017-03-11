05eecf4d90a774f42c14927326ab4677428d4d18
Andrew Sonin <sonin.cel@gmail.com>
2017-03-11 06:07:07 +0300

    Add files via upload

--- vk_friends_graph.py
+++ vk_friends_graph.py
@@ -9,19 +9,19 @@ def get_friends_dict(user_id=1):
     return requests.get(HOST + 'friends.get', params={'user_id': user_id, 'fields': 'first_name', 'v': VERSION}).json()
 
 
-def add_account_to_graph(graph, id):
+def add_account_to_graph(graph, account_id):
     try:
-        for friend in get_friends_dict(id)['response']['items']:
-            graph.add_edge(id, friend['id'])
+        for friend in get_friends_dict(account_id)['response']['items']:
+            graph.add_edge(account_id, friend['id'])
     except KeyError:
         pass
 
 
-def graph_builder(graph, id, depth=1, delay=float(0), inscription='', enrich_inscription=True):
-    add_account_to_graph(graph, id)
+def graph_builder(graph, account_id, depth=1, delay=float(0), inscription='', enrich_inscription=True):
+    add_account_to_graph(graph, account_id)
     sleep(delay)
     if depth == 1:
-        return
+        return None
     if enrich_inscription:
         inscription = str(len(graph.nodes()))
     iterator = 1
@@ -56,7 +56,7 @@ string = [str(i) + ' - ' + str(drawing_ways[i]) for i in range(len(drawing_ways)
 print('\nChoose one of the drawing algorithms:')
 print(*string, sep=', ')
 drawing_way = int(input())
-if not drawing_way in range(len(drawing_ways)):
+if drawing_way not in range(len(drawing_ways)):
     drawing_way = 0
     print('Invalid value. The algorithm was changed to 0.')
 
