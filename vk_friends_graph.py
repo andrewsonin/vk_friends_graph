e93ca3d83df743deb2dbe636030cebffb039797b
Andrew Sonin <sonin.cel@gmail.com>
2018-01-02 03:23:53 +0300

    Add files via upload

--- vk_friends_graph.py
+++ vk_friends_graph.py
@@ -1,11 +1,14 @@
 from requests import get
 from matplotlib.pyplot import axis, savefig, show
-from networkx import *
+from networkx import Graph, draw_networkx_nodes, draw_networkx_labels, draw_networkx_edges, circular_layout, \
+    spring_layout, spectral_layout, random_layout, shell_layout
 from time import sleep
 from os import listdir
 from random import randint
+from argparse import ArgumentParser
+
 HOST = 'https://api.vk.com/method/'
-VERSION = '5.62'
+VERSION = '5.69'
 
 
 def get_friends_dict(user_id=1):
@@ -13,84 +16,89 @@ def get_friends_dict(user_id=1):
 
 
 def add_account_to_graph(graph, account_id, fr_only=False):
-    try:
-        if fr_only:
-            for friend in get_friends_dict(account_id)['response']['items']:
-                if friend['id'] in graph.nodes():
-                    graph.add_edge(account_id, friend['id'])
-        else:
-            for friend in get_friends_dict(account_id)['response']['items']:
+    friends = get_friends_dict(account_id).get('response', {}).get('items', [])
+    if fr_only:
+        for friend in friends:
+            if friend['id'] in graph.nodes():
                 graph.add_edge(account_id, friend['id'])
-    except KeyError:
-        pass
+    else:
+        for friend in friends:
+            graph.add_edge(account_id, friend['id'])
 
 
 def graph_builder(graph, account_id, depth=1, delay=float(0), fr_only=False, inscription='', enrich_inscription=True):
     sleep(delay)
-    if depth == 1:
-        add_account_to_graph(graph, account_id, fr_only)
-        return None
-    else:
+    if depth != 1:
         add_account_to_graph(graph, account_id)
-    if enrich_inscription:
-        inscription = str(len(graph.nodes()) - 1)
-    iterator = 1
-    for friend_id in list(graph.nodes())[1:]:
-        iteration = str(iterator) + '/' + inscription
-        print('Iteration:', iteration)
-        graph_builder(graph, friend_id, depth - 1, delay, fr_only, iteration, False)
-        iterator += 1
+        if enrich_inscription:
+            inscription = str(len(graph.nodes()) - 1)
+        for iter_num, friend_id in enumerate(list(graph.nodes())[1:], 1):
+            iteration = str(iter_num) + '/' + inscription
+            print('Iteration:', iteration)
+            graph_builder(graph, friend_id, depth - 1, delay, fr_only, iteration, False)
+    else:
+        add_account_to_graph(graph, account_id, fr_only)
 
-print('This program is designed to build a graph of the friendship relations between users of social network VK.\n'
-      'Please, input ID of some VK user:')
-my_id = int(input())
 
-print('\nPlease, input a depth of search.\nATTENTION!!! If you choose a depth of search greater than 1, be ready '
-      'to face a lack of RAM.')
-my_depth = int(input())
-if my_depth < 1:
-    my_depth = 1
-    print('Invalid value. The depth was changed to 1.')
+def main():
+    parser = ArgumentParser(description='VK Friend Graph Builder')
 
-print('\nPlease, input a time delay in seconds in range from 0 to 10.\nIf you have a very fast computer, please, input '
-      'nonzero delay. Otherwise antiDoS system may reject you.')
-my_delay = float(input())
-if not 0 <= my_delay <= 10:
-    my_delay = 0.3
-    print('Invalid value. Time delay was changed to 0.3 second.')
+    parser.add_argument('user_id',
+                        metavar='USER_ID',
+                        type=int,
+                        nargs=1,
+                        help='VK Id of the person of interest')
 
-drawing_ways = ['circular', 'spring', 'spectral', 'random', 'shell']
-string = [str(i) + ' - ' + str(drawing_ways[i]) for i in range(len(drawing_ways))]
-print('\nChoose one of the drawing algorithms:')
-print(*string, sep=', ')
-drawing_way = int(input())
-if drawing_way not in range(len(drawing_ways)):
-    drawing_way = 0
-    print('Invalid value. The algorithm was changed to 0.')
+    parser.add_argument('-d', '--dep', type=int, action='store', nargs='?', default=2,
+                        help='Depth of search. ATTENTION!!! If you choose a depth of search greater than 1, '
+                             'be ready to face a lack of RAM.')
+    parser.add_argument('-s', '--slp', type=float, action='store', nargs='?', default=0.0,
+                        help='Time delay in seconds in range from 0 to 10 (if larger, it is interpreted as 10). '
+                             'Default is 0. If you have a very fast Internet connection, '
+                             'please, input nonzero delay. Otherwise antiDoS system may reject you.')
+    parser.add_argument('-a', '--alg', type=int, action='store', nargs='?', default=1,
+                        help='Drawing algorithms: 0 - circular, 1 - spring, 2 - spectral, 3 - random, 4 - shell. Other '
+                             'and default is spring.')
+    parser.add_argument('-m', '--mod', type=int, action='store', nargs='?', default=1,
+                        help='Operating mode: 1 - At the last step of the recursion program adds edges to the graph '
+                             'only between already included vertexes; any other - At the last step of the recursion '
+                             'program adds all incoming edges to the graph.')
 
-print('\nPlease, choose operating mode:')
-print('1 - At the last step of the recursion program will add edges to graph only between already included vertexes;'
-      '\nany other - At the last step of the recursion program will add to graph all incoming edges.')
-friends_only = (lambda x: True if x == '1' else False)(input())
+    args = parser.parse_args()
 
-my_graph = Graph()
-graph_builder(my_graph, my_id, my_depth, my_delay, friends_only)
+    my_graph = Graph()
+    graph_builder(my_graph, args.user_id[0], args.dep, args.slp, True if args.mod == 1 else False)
 
-print('\nCalculating space configuration of vertexes…')
-positions = eval(drawing_ways[drawing_way] + '_layout(my_graph)')
+    print('\nPlease, wait. Calculating space configuration of vertexes…')
+    if args.alg == 1:
+        positions = spring_layout(my_graph)
+    elif args.alg == 0:
+        positions = circular_layout(my_graph)
+    elif args.alg == 2:
+        positions = spectral_layout(my_graph)
+    elif args.alg == 3:
+        positions = random_layout(my_graph)
+    elif args.alg == 4:
+        positions = shell_layout(my_graph)
+    else:
+        positions = spring_layout(my_graph)
 
-print('Full number of accounts involved:', len(my_graph.nodes()))
-print('Drawing a graph…')
-draw_networkx_nodes(my_graph, positions, node_shape='s', node_size=500, node_color='y')
-draw_networkx_labels(my_graph, positions, font_size=4, font_family='sans-serif', font_color='r')
-draw_networkx_edges(my_graph, positions, edgelist=my_graph.edges(), width=1)
+    print('Full number of accounts involved:', my_graph.number_of_nodes())
+    print('Drawing a graph…')
+    draw_networkx_nodes(my_graph, positions, node_shape='s', node_size=40, node_color='y')
+    draw_networkx_labels(my_graph, positions, font_size=1, font_family='sans-serif', font_color='r')
+    draw_networkx_edges(my_graph, positions, edgelist=my_graph.edges(), width=0.1)
 
-axis('off')
-fig_name, dir_files = '', set(listdir('.'))
-while not fig_name:
-    variant = str(randint(1000000, 9999999)) + '.png'
-    if variant not in dir_files:
-        fig_name = variant
-savefig(fig_name)
-print('The image was saved as', fig_name)
-show()
+    axis('off')
+    fig_name, dir_files = '', set(listdir('.'))
+    while not fig_name:
+        variant = str(randint(1000000, 9999999)) + '.pdf'
+        if variant not in dir_files:
+            fig_name = 'id' + str(args.user_id[0]) + '_img' + variant
+    savefig(fig_name, format='pdf')
+    print('The image was saved as', fig_name)
+    show()
+
+
+if __name__ == '__main__':
+    main()
